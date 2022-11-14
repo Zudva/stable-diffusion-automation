9247b2328cf9e4e978789e4f899de02da08e0ca4
Vyacheslav Zudvin <zudva@protonmail.com>
2022-11-14 09:25:56 +0600

    Add remove.sh

---
+++ remove.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Start autoremove"
+
+sleep 1
+
+rm -rf ./content
+rm -rf ./flow_tools
+rm -rf ./RobustVideoMattingCLI
+rm -rf ./WarpFusion
+rm -rf ./content
+rm -rf ./flow_tools
+rm -rf ./guided-diffusion
+rm -rf ./images_out
+rm -rf ./init_images
+rm -rf ./k-diffusion
+rm -rf ./models
+rm -rf ./python-color-transfer  
+rm -rf ./src
+rm -rf ./stable-diffusion 
+
+sleep 0.5
+
+echo "All detele"
+
+exit
